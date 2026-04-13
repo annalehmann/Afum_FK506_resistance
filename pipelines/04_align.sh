b500335a3d4980abd6cfce43b7c2258d8cf652fb
Anna Lehmann <66916817+annalehmann@users.noreply.github.com>
2026-04-13 14:35:26 -0400

    Update 04_align.sh

--- pipelines/04_align.sh
+++ pipelines/04_align.sh
@@ -6,7 +6,7 @@ minimap2 \
 CEA10_genome.fa \
 "$n"_R1_001.fastq.gz \
 "$n"_R2_001.fastq.gz |\
-samtools view -q 30 |\
+samtools view -h -q 30 |\
 samtools sort -o "$n"_filteredq30.bam
 samtools index "$n"_filteredq30.bam
 done
